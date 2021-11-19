2ee728d8e91536a4dcf378e4fe8223030372f57e
Surya Venkatesan <suryaedu71@gmail.com>
2021-11-19 22:07:21 +0530

    ADDED dashboard

--- app/src/main/java/com/sparrow/techathon/LoginScreen.kt
+++ app/src/main/java/com/sparrow/techathon/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.sparrow.techathon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,11 @@ class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
+        val mbtn_signIn = findViewById<com.google.android.material.button.MaterialButton>(R.id.mbtn_signIn)
+        mbtn_signIn.setOnClickListener {
+            val intent = Intent(this,dashboardActivity::class.java)
+            intent.flags  = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+        }
     }
 }
